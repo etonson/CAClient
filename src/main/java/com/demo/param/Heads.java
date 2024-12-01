7f72103935a269cd634dc2bb104d4bf5c0b99442
eton052096@gmail.com <eton052096@gmail.com>
2024-12-02 01:55:09 +0800

    加入攤平JSON相關工具包

--- src/main/java/com/demo/param/Heads.java
+++ src/main/java/com/demo/param/Heads.java
@@ -12,11 +12,11 @@ public enum Heads {
     XML(0, "text/xml;charset=utf-8"),
     JSON(1, "application/json;charset=utf-8");
 
-    private int no;
-    private String method;
+    private final int NO;
+    private final String METHOD;
 
     Heads(int no, String method) {
-        this.no = no;
-        this.method = method;
+        this.NO = no;
+        this.METHOD = method;
     }
 }
